0a57a3d980678b30769a3f4f091c29e4a3fc1d94
ltrcoin <40208286+ltrcoin@users.noreply.github.com>
2018-10-02 21:43:38 +0700

    Add buytickets api

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/getbalance', [
+            'as' => 'frontend.api.getbalance',
+            'uses'  => 'APISiteController@getbalance'
+        ]);
+Route::post('/buytickets', [
+            'as' => 'frontend.api.buytickets',
+            'uses'  => 'APISiteController@buytickets'
+        ]);
 Route::get('/gamesinfo', [
-            'as' => 'frontend.site.vapiLogin',
+            'as' => 'frontend.api.gamesinfo',
             'uses'  => 'APISiteController@gamesinfo'
         ]);
 
+Route::get('/winningnumbers', [
+            'as' => 'frontend.api.winningnumbers',
+            'uses'  => 'APISiteController@winningnumbersapi'
+        ]);
+
 Route::post('/login', [
-			'as' => 'frontend.site.vapiLogin',
+			'as' => 'frontend.api.login',
 			'uses'  => 'APISiteController@apilogin'
 		]);
 
@@ -36,8 +49,13 @@ Route::get('/login', function (Request $request) {
     		]);
 });
 	
+Route::post('/validateregister', [
+            'as' => 'frontend.api.validateregister',
+            'uses'  => 'APISiteController@validateregister'
+        ]);
+
 Route::post('/register', [
-            'as' => 'frontend.site.vapiregister',
+            'as' => 'frontend.api.register',
             'uses'  => 'APISiteController@apiregister'
         ]);
 
